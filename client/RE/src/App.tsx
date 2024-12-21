53f5f0523b5596407df8ba01f4aefd00c5767271
BlueMoon522 <nabinnabin550@gmail.com>
2024-12-21 16:42:37 +0545

    added a signup and login page frontend

--- client/RE/src/App.tsx
+++ client/RE/src/App.tsx
@@ -2,14 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Layout from "./layout";
+import Login from "./pages/login/page.tsx";
+import Signup from "./pages/signup/page.tsx";
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element=<Home /> />
-        <Route path="/about" element=<About /> />
-      </Routes>
+      <div className="flex items-center justify-center h-screen">
+        <Routes>
+          <Route path="/" element=<Home /> />
+          <Route path="/signup" element=<Signup /> />
+          <Route path="/login" element=<Login /> />
+          <Route path="/about" element=<About /> />
+        </Routes>
+      </div>
     </Layout>
   );
 }
--- client/RE/src/components/app-sidebar.tsx
+++ client/RE/src/components/app-sidebar.tsx
@@ -1,56 +1,38 @@
-import * as React from "react"
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react"
+import * as React from "react";
+import { Bot, Frame, Map, PieChart, SquareTerminal } from "lucide-react";
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
+import { NavMain } from "@/components/nav-main";
+import { Navtitles } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 // This is sample data.
 const data = {
   user: {
-    name: "shadcn",
+    name: "JackTheJacker",
     email: "m@example.com",
     avatar: "/avatars/shadcn.jpg",
   },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
   navMain: [
     {
-      title: "Playground",
+      title: "Navigation",
+      url: "#",
+      icon: SquareTerminal,
+      items: [
+        {
+          title: "Home",
+          url: "/",
+        },
+        //add more to add to the list of links link
+      ],
+    },
+    {
+      title: "Favourite",
       url: "#",
       icon: SquareTerminal,
       isActive: true,
@@ -60,17 +42,17 @@ const data = {
           url: "#",
         },
         {
-          title: "Starred",
+          title: "Mystery",
           url: "#",
         },
         {
-          title: "Settings",
+          title: "Fishery",
           url: "#",
         },
       ],
     },
     {
-      title: "Models",
+      title: "All",
       url: "#",
       icon: Bot,
       items: [
@@ -88,54 +70,8 @@ const data = {
         },
       ],
     },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
   ],
-  projects: [
+  titles: [
     {
       name: "Design Engineering",
       url: "#",
@@ -152,22 +88,19 @@ const data = {
       icon: Map,
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <NavUser user={data.user} />
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <Navtitles titles={data.titles} />
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
