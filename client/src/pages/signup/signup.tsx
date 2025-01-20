9a75eb362e5170ea02accecf3fbb9f2e90e6381a
BlueMoon522 <nabinnabin550@gmail.com>
2025-01-20 14:33:12 +0545

    Fixed some type error

--- client/src/pages/signup/signup.tsx
+++ client/src/pages/signup/signup.tsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useNavigate } from "react-router-dom";
