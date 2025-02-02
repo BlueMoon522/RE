8b89c2fa6c47a2c2cfeb244c086485dc99eb178d
BlueMoon522 <nabinnabin550@gmail.com>
2025-02-02 17:38:30 +0545

    created a top bar component

--- front-end/src/pages/test/page.jsx
+++ front-end/src/pages/test/page.jsx
@@ -1,7 +1,9 @@
 import Toggle from "../../components/common/toggle/toggle";
+import TopBar from "../../components/common/topbar/topbar";
 export default function Test() {
   return (
     <>
+      <TopBar />
       <Toggle />
     </>
   );
