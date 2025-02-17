38a8193c541fe184a49f943462bec25fa62e458c
BlueMoon522 <nabinnabin550@gmail.com>
2025-02-17 17:29:59 +0545

    unnecessary commit

--- server/routes/content.routes.js
+++ server/routes/content.routes.js
@@ -12,6 +12,7 @@ import { protectedRoutes } from "../middleware/protectedRoutes.js";
 const router = express();
 
 router.post("/:id", protectedRoutes, postContent);
+router.post("/:id", protectedRoutes, postContent); //add the subcontent,here the id of the content not the subcontent
 router.post("/update/:id", protectedRoutes, updateContent);
 router.get("/", getAllContent); //gets all the contents on the content DB
 router.get("/get/:id", getCurrentContent); //get the content based on the contentID
